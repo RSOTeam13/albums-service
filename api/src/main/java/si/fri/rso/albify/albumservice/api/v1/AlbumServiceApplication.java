64c87480d14b08d5cb154903650609b0a0888fd0
Anze Mur <am3158@student.uni-lj.si>
2021-01-06 01:25:41 +0100

    Fixed filters.

--- api/src/main/java/si/fri/rso/albify/albumservice/api/v1/AlbumServiceApplication.java
+++ api/src/main/java/si/fri/rso/albify/albumservice/api/v1/AlbumServiceApplication.java
@@ -1,9 +1,25 @@
 package si.fri.rso.albify.albumservice.api.v1;
 
+import si.fri.rso.albify.albumservice.api.v1.filters.IncomingRequestFilter;
+import si.fri.rso.albify.albumservice.api.v1.resources.AlbumResource;
+import si.fri.rso.albify.albumservice.services.filters.AuthenticationFilter;
+
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
+import java.util.HashSet;
+import java.util.Set;
 
 @ApplicationPath("/v1")
 public class AlbumServiceApplication extends Application {
+
+    @Override
+    public Set<Class<?>> getClasses() {
+        final Set<Class<?>> resources = new HashSet<Class<?>>();
+        resources.add(IncomingRequestFilter.class);
+        resources.add(AlbumResource.class);
+        resources.add(AuthenticationFilter.class);
+
+        return resources;
+    }
 
 }
